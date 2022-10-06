fcce08a33a33fde098f03ee573f77029690e4c3d
Ivan Onishchenko <Lightsout07@mail.ru>
2022-10-06 18:32:27 +0300

    15 select component end

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -2,32 +2,73 @@ import React, { ChangeEvent, useState } from "react";
 import { Select } from "./Select";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import { action, actions } from "@storybook/addon-actions";
+
+// my variant
+// export default {
+//   title: "Select",
+// };
+//
+// export const Selected = () => {
+//   const [city, setCity] = useState<string | undefined>("");
+//
+//   const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+//     setCity(e.currentTarget.value);
+//   };
+//   return (
+//     <FormControl sx={{ m: 1, minWidth: 180 }}>
+//       <InputLabel>{city}</InputLabel>
+//       <Select
+//         value={"Cities"}
+//         onChange={onChange}
+//         items={[
+//           { title: "None", value: 0 },
+//           { title: "Moscow", value: 1 },
+//           { title: "Sevastopol", value: 2 },
+//           { title: "Minsk", value: 3 },
+//           { title: "Krasnodar", value: 4 },
+//           { title: "Praga", value: 5 },
+//         ]}
+//       />
+//     </FormControl>
+//   );
+// };
 
 export default {
   title: "Select",
+  component: Select,
 };
 
-export const Selected = () => {
-  const [city, setCity] = useState<string | undefined>("");
-
-  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setCity(e.currentTarget.value);
-  };
+export const WithValue = () => {
+  const [value, setValue] = useState("2");
   return (
-    <FormControl sx={{ m: 1, minWidth: 180 }}>
-      <InputLabel>{city}</InputLabel>
+    <>
       <Select
-        value={"Cities"}
-        onChange={onChange}
+        onChange={setValue}
+        value={value}
         items={[
-          { title: "None", value: 0 },
-          { title: "Moscow", value: 1 },
-          { title: "Sevastopol", value: 2 },
-          { title: "Minsk", value: 3 },
-          { title: "Krasnodar", value: 4 },
-          { title: "Praga", value: 5 },
+          { value: "1", title: "Minsk" },
+          { value: "2", title: "Moscow" },
+          { value: "3", title: "Kiev" },
         ]}
       />
-    </FormControl>
+    </>
+  );
+};
+
+export const WithoutValue = () => {
+  const [value, setValue] = useState(null);
+  return (
+    <>
+      <Select
+        onChange={setValue}
+        value={value}
+        items={[
+          { value: "1", title: "Minsk" },
+          { value: "2", title: "Moscow" },
+          { value: "3", title: "Kiev" },
+        ]}
+      />
+    </>
   );
 };
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,21 +1,91 @@
+import s from "./Select.module.css";
+import { useState, KeyboardEvent, useEffect } from "react";
+
 export type ItemType = {
   title: string;
   value: any;
 };
 
 export type SelectPropsType = {
-  value: any;
+  value?: any;
   onChange: (value: any) => void;
   items: ItemType[];
 };
+// my variant
+// export function Select(props: SelectPropsType) {
+//   return (
+//     <select>
+//       <div>{props.value}</div>
+//       {props.items.map((i, index) => (
+//         <option key={props.value}>{i.title}</option>
+//       ))}
+//     </select>
+//   );
+// }
 
 export function Select(props: SelectPropsType) {
+  const [active, setActive] = useState(false);
+  const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+
+  const selectedItem = props.items.find((i) => i.value === props.value);
+  const hoveredItem = props.items.find((i) => i.value === hoveredElementValue);
+
+  useEffect(() => {
+    setHoveredElementValue(props.value);
+  }, [props.value]);
+
+  const toggleItems = () => setActive(!active);
+  const onItemClick = (value: any) => {
+    props.onChange(value);
+    toggleItems();
+  };
+
+  const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+      for (let i = 0; i < props.items.length; i += 1) {
+        if (props.items[i].value === hoveredElementValue) {
+          const pretendentElement =
+            e.key === "ArrowDown" ? props.items[i + 1] : props.items[i - 1];
+          if (pretendentElement) {
+            props.onChange(pretendentElement.value);
+            return;
+          }
+        }
+      }
+      if (!selectedItem) {
+        props.onChange(props.items[0].value);
+      }
+    }
+
+    if (e.key === "Enter" || e.key === "Escape") {
+      setActive(false);
+    }
+  };
   return (
-    <select>
-      <div>{props.value}</div>
-      {props.items.map((i, index) => (
-        <option key={props.value}>{i.title}</option>
-      ))}
-    </select>
+    <>
+      <div className={s.select} onKeyUp={onKeyUp} tabIndex={0}>
+        <span className={s.main} onClick={toggleItems}>
+          {selectedItem && selectedItem.title}
+        </span>
+        {active && (
+          <div className={s.items}>
+            {props.items.map((i) => (
+              <div
+                onMouseEnter={() => {
+                  setHoveredElementValue(i.value);
+                }}
+                className={s.item + " " + (hoveredItem === i ? s.selected : "")}
+                key={i.value}
+                onClick={() => {
+                  onItemClick(i.value);
+                }}
+              >
+                {i.title}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
